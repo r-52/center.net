884717d777f008bf5e05f8b6a5b3aa1d9217445a
Roman <romaaan.git@gmail.com>
2022-06-13 19:17:36 +0000

    chore: fix typo :/

--- src/WebUI/ClientApp/src/app/material.module.ts
+++ src/WebUI/ClientApp/src/app/material.module.ts
@@ -10,7 +10,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import { MatCardModule } from '@angular/material/card/';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [],
   imports: [

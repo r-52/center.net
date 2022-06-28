a8434624bf259ad298af1f6be176bb896f026f72
Roman <romaaan.git@gmail.com>
2022-06-28 19:23:15 +0000

    chore: cleanup start / home component...

--- src/WebUI/ClientApp/src/app/home/home.component.ts
+++ src/WebUI/ClientApp/src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../shared/components/base/base.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent extends BaseComponent {}

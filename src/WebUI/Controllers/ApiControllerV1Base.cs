9040cacc6a74b60f64810b58c13cad43f46dc811
Roman <romaaan.git@gmail.com>
2022-07-11 19:19:31 +0000

    chore: provide helper methods to get the current user

--- src/WebUI/Controllers/ApiControllerV1Base.cs
+++ src/WebUI/Controllers/ApiControllerV1Base.cs
@@ -1,3 +1,7 @@
+using System.Security.Claims;
+using HumanCenterNet.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+
 namespace HumanCenterNet.WebUI.Controllers;
 
 
@@ -7,8 +11,21 @@ public abstract class ApiControllerV1Base : ControllerBase {
 
     protected ILogger _logger;
 
-    public ApiControllerV1Base(ILogger logger)
+    protected readonly UserManager<ApplicationUser> _userManager;
+
+
+    public ApiControllerV1Base(ILogger logger, UserManager<ApplicationUser> userManager)
     {
         _logger = logger;
+        _userManager = userManager;
+    }
+
+    protected ClaimsPrincipal GetUser() {
+        return this.User;
+    }
+
+    protected async Task<ApplicationUser> GetDbUser() {
+        var fullUser =  await _userManager.GetUserAsync(GetUser());
+        return fullUser;
     }
 }
